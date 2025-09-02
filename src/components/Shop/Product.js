71937891766f252966dce43e00bc31d9ae259451
erfan-ne <erfan.neshati@gmail.com>
2025-09-02 15:19:55 +0330

    making Dynamic product addition

--- src/components/Shop/Product.js
+++ src/components/Shop/Product.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react'
 
 export default class Product extends Component {
     
+    // constructor(props){
+    //     super(props)
+
+    //     const props = ...props
+    // }
+
     render() {
         return (
             <div className="shop-item">
-                <span className="shop-item-title">Shirt</span>
-                <img className="shop-item-image" src='Images/Shirt.png' alt='shirt'/>
+                <span className="shop-item-title">{this.props.title}</span>
+                <img className="shop-item-image" src={this.props.img} alt={this.props.title}/>
                 <div className="shop-item-details">
-                    <span className="shop-item-price">$129</span>
+                    <span className="shop-item-price">{this.props.price} $</span>
                     <button
                         className="btn btn-primary shop-item-button"
                         type="button">
