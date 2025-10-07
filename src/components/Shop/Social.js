72ffe20698e0560235f733acd5505bf18673bd83
erfan-ne <erfan.neshati@gmail.com>
2025-10-07 17:52:40 +0330

    refactor(social.js)

--- src/components/Shop/Social.js
+++ src/components/Shop/Social.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Social extends Component {
-    render() {
+export default function Social () {
 
-        let {href, img} = this.props
 
-        return (
-            <li>
-                <a href={href} target="_blank">
-                    <img src={img} alt='social' />
-                </a>
-            </li>
-        )
-    }
+    let {href, img} = this.props
+    return (
+        <li>
+            <a href={href} target="_blank">
+                <img src={img} alt='social'/>
+            </a>
+        </li>
+    )
 }
