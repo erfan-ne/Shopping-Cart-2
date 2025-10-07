d9078571e5147dadabc0498fb6846b1b4c5e10da
erfan-ne <erfan.neshati@gmail.com>
2025-10-07 17:34:43 +0330

    refactor(App.js): start refactoring

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Shop from './components/Shop/Shop'
 import './App.css'
 
+//start refactoring to functional components
 export default class App extends Component {
 
     render() {
