50168d2e5799599d89fd4138ee9f9f171a612fd3
erfan-ne <erfan.neshati@gmail.com>
2025-10-07 18:04:29 +0330

    refactor(Product.js)

--- src/components/Shop/Product.js
+++ src/components/Shop/Product.js
@@ -1,29 +1,26 @@
 import React from 'react'
 
-export default function Product () {
+export default function Product (props) {
 
-    const clickHandler(){
-        this.props.onAddToCart(this.props)
+    const clickHandler = () => {
+        props.onAddToCart(props)
     }
 
-    render() {
+    let {title, img, price} = props
 
-        let {title, img, price} = this.props
-
-        return (
-            <div className="shop-item">
-                <span className="shop-item-title">{title}</span>
-                <img className="shop-item-image" src={img} alt={title}/>
-                <div className="shop-item-details">
-                    <span className="shop-item-price">{price} $</span>
-                    <button
-                        className="btn btn-primary shop-item-button"
-                        type="button"
-                        onClick={this.clickHandler}>
-                        ADD TO CART
-                    </button>
-                </div>
+    return (
+        <div className="shop-item">
+            <span className="shop-item-title">{title}</span>
+            <img className="shop-item-image" src={img} alt={title}/>
+            <div className="shop-item-details">
+                <span className="shop-item-price">{price} $</span>
+                <button
+                    className="btn btn-primary shop-item-button"
+                    type="button"
+                    onClick={clickHandler}>
+                    ADD TO CART
+                </button>
             </div>
-        )
-    }
+        </div>
+    )
 }
