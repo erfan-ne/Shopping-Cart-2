fd5b439338e25eca6559bf83df64163138817bb5
erfan-ne <erfan.neshati@gmail.com>
2025-10-07 17:36:51 +0330

    refactor(App.js)

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import Shop from './components/Shop/Shop'
 import './App.css'
 
 //start refactoring to functional components
-export default class App extends Component {
-
-    render() {
-        return (
-            <div>
-                <Shop />
-            </div>
-        )
-    }
+export default function App () {
+    return (
+        <div>
+            <Shop />
+        </div>
+    )
 }
